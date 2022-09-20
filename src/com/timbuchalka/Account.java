cb5e6bebb2f0749fdad4295bde59ec7e7423259d
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-20 12:54:43 -0400

    Implemented getters and setters in the Account file

--- src/com/timbuchalka/Account.java
+++ src/com/timbuchalka/Account.java
@@ -1,6 +1,45 @@
 package com.timbuchalka;
 
 public class Account {
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
 
     private String number;
     private double balance;
