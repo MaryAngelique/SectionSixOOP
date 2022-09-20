2fdae2470e244df1f35344e90ac6699cc0b3af2f
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-20 12:37:53 -0400

    Implemented multiple private methods

---
+++ src/com/timbuchalka/Account.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+public class Account {
+
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+
+}
